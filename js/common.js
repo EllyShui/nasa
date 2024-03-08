21debb483102c0ca5a3aef2d1ed11b1b98f2098c
EllyShui <shui415@gmail.com>
2024-03-08 14:54:53 +0900

    2nd commit
    header search - tablet
    imgday number

--- js/common.js
+++ js/common.js
@@ -8,41 +8,15 @@ $(document).ready(function(){
     });
 
     // 반응형
-    var w_width = $(window).width();
+    var w_width = $(window).width() + 17;
     if(w_width > 768 && w_width <= 1440){ // 태블릿
-        // right_menu 검색
-        // $('#search_in').click(function(){
-        //     let search_style = this.style;
-        //     $(this).css({
-        //         'width':'100%',
-        //         'height':'100px',
-        //         'border-radius':'10px',
-        //         'padding':'0 8px',
-        //         'line-height':'30px',
-        //         'background':'#292929',
-        //         'color':'#f5f5f5',
-        //         'text-align':'center',
-        //         'font-size':'22px',
-        //         'position':'absolute',
-        //         'top':'110%',
-        //         'left':'0',
-        //         'transition':'0.6s'
-        //     });
-        //     $('.search img').click(function(){
-        //         $('#search_in').css({
-        //             'width':'160px',
-        //             'height':'22px',
-        //             'margin-right':'5px',
-        //             'border-radius':'4px',
-        //             'padding':'0 4px',
-        //             'line-height':'20px',
-        //             'background':'rgba(0,0,0,0)',
-        //             'color':'#dddddd',
-        //             'font-size':'18px',
-        //             'text-align':'right'
-        //         });
-        //     });
-        // });
+        // search
+        $('#search_in').focus(function(){
+            $(this).addClass('search_mag');
+        });
+        $('#search_in').blur(function(){
+            $(this).removeClass('search_mag');
+        });
     }
     if(w_width <= 768){ // 모바일
         // 서브메뉴
--- js/imgday.js
+++ js/imgday.js
@@ -1,74 +1,76 @@
 $(document).ready(function(){
-    let num = $('.num').text(),
-        num_page,
-        num_idx;
-    for(let i=0;i<num.length;i++){
-        num_page = parseInt((Number(num[i])-1)/4);
-    }
-    console.log(num_page[0]);
-    // dp_img change
-    $('.page li').click(function(){
-        $('.dp_img').find('img').remove();
-        $(this).find('img').clone().appendTo('.dp_img>div:first-child');
-    });
 
-    // num_btn
+    let num, //1,2,3,4,5,6,7,8
+        num_page; //1234>0,5678>1
+
+    // 페이지 숫자 클릭
     $('.num').click(function(){
         num = Number($(this).text());
-        num_idx = parseInt((num-1)/4);
+        num_page = parseInt((num-1)/4);
 
         $(this).addClass('num_on').siblings('.num').removeClass('num_on');
         img_change();
+        arrow();
     });
-    
-    // arrow > page_num 넘어가게
+    // 화살표 클릭
     $('.arrow_right').click(function(){
         num = Number($('.num_on').text());
-        page_num = parseInt((num-1)/4);
+        num_page = parseInt((num-1)/4) + 1;
 
-        page_num += 1;
-        num = page_num*4 + 1;
-        num_change();
+        num = (num_page)*4 + 1;
+
+        $('.num').eq(num-1).addClass('num_on').siblings('.num').removeClass('num_on');
+        img_change();
+        numpage();
+        arrow();
+    });
+    $('.arrow_left').click(function(){
+        num = Number($('.num_on').text());
+        num_page = parseInt((num-1)/4) - 1;
+
+        num = (num_page)*4 + 1;
+
+        $('.num').eq(num-1).addClass('num_on').siblings('.num').removeClass('num_on');
+        img_change();
+        numpage();
+        arrow();
     });
 
 
 
 
 
-
-
-
-
-    function img_change(){
-        // page li change
+    function numpage(){ // numpage change
+        $('.num_btn li').hide();
+        for(let i = num_page*4; i<(num_page*4)+4; i++){
+            $('.num_btn li').eq(i).show();
+        }
+    }
+    function img_change(){ // page li change
         $('.page li').hide();
         for(let i=0;i<15;i++){
             $('.page li').eq((15*(num - 1))+i).show();
         }
-
-        // arrow color
-        $('.arrow_left path').css({'stroke':'#696969'});
-        $('.arrow_right path').css({'stroke':'#696969'});
-        $('.arrow').css({'pointer-events':'auto'});
-        $('.dots').show();
-        if(page_num == 1){
+    }
+    function arrow(){ // arrow color
+        if(num_page == 0){
             $('.arrow_left').css({'pointer-events':'none'});
             $('.arrow_right').css({'pointer-events':'auto'});
             $('.arrow_left path').css({'stroke':'#dddddd'});
             $('.arrow_right path').css({'stroke':'#696969'});
-        }else if(page_num == parseInt((Number($('.num:last-child').text())-1)/4)){
+            $('.dots').show();
+        }else if(num_page == parseInt((Number($('.num:last-child').text())-1)/4)){
             $('.arrow_left').css({'pointer-events':'auto'});
             $('.arrow_right').css({'pointer-events':'none'});
             $('.arrow_left path').css({'stroke':'#696969'});
             $('.arrow_right path').css({'stroke':'#dddddd'});
             $('.dots').hide();
+        }else{
+            $('.arrow').css({'pointer-events':'auto'});
+            $('.arrow_left path').css({'stroke':'#696969'});
+            $('.arrow_right path').css({'stroke':'#696969'});
+            $('.dots').show();
         }
     }
 
-    function num_change(){
-        $('.num_btn li').hide();
-        for(let i = page_num*4; i<(page_num*4)+4; i++){
-            $('.num_btn li').eq(i).show();
-        }
-    }
 });
